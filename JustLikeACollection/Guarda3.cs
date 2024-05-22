2e282171d19a536a5e6ca107bad3b43224398a34
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-22 21:05:11 +0100

    add JustLikeACollection proj to sln

---
+++ JustLikeACollection/Guarda3.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T _var1;
+        private T _var2;
+        private T _var3;
+
+        public Guarda3()
+        {
+            _var1 = default;
+            _var2 = default;
+            _var3 = default;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return _var1;
+            yield return _var2;
+            yield return _var3;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+        public T GetItem(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return _var1;
+                case 1:
+                    return _var2;
+                case 2:
+                    return _var3;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void SetItem(int i, T item)
+        {
+            switch (i)
+            {
+                case 0:
+                    _var1 = item;
+                    break;
+                case 1:
+                    _var2 = item;
+                    break;
+                case 2:
+                    _var3 = item;
+                    break;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
