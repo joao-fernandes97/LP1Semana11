d4205ea28efaacf266e1eb939dfd60ac6c72759b
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-22 20:28:42 +0100

    Checker class and test Program

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+    {
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            int count = 0;
+            foreach (object item in items)
+            {
+                if (item is T)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
