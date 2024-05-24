14c4ccec2e0aead749e979c5dd33db71cddf6627
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-24 16:03:04 +0100

    add MyOwnCollection proj to sln

---
+++ MyOwnCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyOwnCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
